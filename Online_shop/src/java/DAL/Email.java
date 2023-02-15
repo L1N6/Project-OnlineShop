e76dcad7bf18e51c20b37c0c3fb26c886703d500
Thanh Tung <thanhtungf21@gmail.com>
2023-02-15 14:44:34 +0700

    merge contact

---
+++ Online_shop/src/java/DAL/Email.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAL;
+
+/**
+ *
+ * @author thanh
+ */
+public class Email {
+    String name;
+    String email;
+    String message;
+
+    public Email() {
+    }
+
+    public Email(String name, String email, String message) {
+        this.name = name;
+        this.email = email;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
